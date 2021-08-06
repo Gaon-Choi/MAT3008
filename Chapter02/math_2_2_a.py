dc4ad7ef112b6833cd297922f151adc65ffcd150
Gaon Choi <choigaon1028@gmail.com>
2021-08-06 19:38:55 +0900

    chapter02

---
+++ Chapter02/math_2_2_a.py
@@ -0,0 +1,32 @@
+# 2019009261_Gaon Choi
+import math
+
+def f(x):
+    return math.log(x)
+
+a = 0.01
+b = 2.0
+accuracy = 100
+count = 1
+
+f_a = f(a); f_b = f(b)
+c_old = b - (f(b)/(f(b)-f(a))) * (b - a)
+f_c = f(c_old)
+print("#", count)
+print("a =", round(a, 5), "b =", round(b, 5), "c_old =", round(c_old, 5), "\n")
+
+while True:
+    count += 1
+    print("#", count)
+    # selection between two interval
+    if(f_a * f_c < 0): b = c_old
+    elif(f_c * f_b < 0): a = c_old
+    print("a =", round(a, 5), "b =", round(b, 5), "c_old =", round(c_old, 5))
+
+    c_new = b - (f(b)/(f(b)-f(a))) * (b - a)
+    print("c_new =", round(c_new, 5))
+    accuracy = abs(((c_new - c_old)/c_new)) * 100
+    c_old = c_new
+    print("Accuracy: ", accuracy)
+    if(accuracy < 0.0005): break
+    print()
---
+++ Chapter02/math_2_3_1_b.py
@@ -0,0 +1,48 @@
+# 2019009261_Gaon Choi
+import math
+import numpy as np
+
+def f(x):
+    return 10 * math.exp(-x) * math.sin(2 * math.pi * x) - 2
+
+def select(x, a, b, c):
+    if b >= 0:
+        return x - ((2 * c) / (b + math.sqrt(b**2 - 4*a*c)))
+    else:   # b < 0
+        return x - ((2 * c) / (b - math.sqrt(b**2 - 4*a*c)))
+
+accuracy = math.inf
+count = 0
+x1 = 0.4; x2 = 0.5; x0 = 0.45
+
+
+while True:
+    print("x1 x0 x2", x1, x0, x2)
+    f1 = f(x1)
+    f2 = f(x2)
+    f0 = f(x0)
+    print("f1 f2 f0", f1, f2, f0)
+    h1 = x0 - x1; h2 = x2 - x0; r = h2 / h1
+    print("h1 h2 r", h1, h2, r)
+
+    a = (r*f1-f0*(1+r)+f2)/(r*h1*h1*(1+r))
+    b = (a*h1*h1+f0-f1)/h1
+    c = f0
+    print("a b c", a, b, c)
+
+    x3_temp = x0
+    x0_temp = x0
+    x3 = select(x0, a, b, c)
+    print(x3)
+    print(np.floor(x3 * (10**5))/(10**5))
+
+    if x3 < x0:
+        x0 = x3; x2 = x0_temp
+    else:
+        x0 = x3; x1 = x0_temp
+    print("x1 x0 x2", x1, x0, x2)
+    accuracy = abs((x3 - x0_temp)/x3) * 100
+    print("Accuracy: ", accuracy)
+    print()
+    if(accuracy < 0.00005):
+        break
